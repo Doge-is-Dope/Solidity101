f74ec81ffb7c0426bb7ee859966bffe442580cec
clement <acf4rxh03@mozmail.com>
2023-03-25 12:36:13 +0800

    feat: add state variables

---
+++ L1StateVariables.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// State Variables
+contract L1StateVariables {
+    uint num = 123; // state variable
+
+    function foo() external {
+        // uint local = 456; // local variable
+    }
+}
