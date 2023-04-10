88415054e6e389cd230a8bedab561a0c3fb7e4b1
Doge-is-Dope <maple3312936@outlook.com>
2023-04-10 23:19:15 +0800

    feat: update ERC20 by adding wrapped eth interface

--- apps/erc20/MyToken.sol
+++ apps/erc20/MyToken.sol
@@ -2,23 +2,29 @@
 pragma solidity ^0.8.19;
 
 import "./IERC20.sol";
+import "./IERC20Metadata.sol";
+import "./IWETH9.sol";
 
-contract MyToken is IERC20 {
-    uint256 public totalSupply; // total amount of tokens have been minted.
-
+contract MyToken is IERC20, IERC20Metadata {
     mapping(address => uint256) public balanceOf; // owner -> amount
     mapping(address => mapping(address => uint256)) public allowance; // owner -> sender(spender) -> amount
 
-    string public name = "My Token"; // name of the token
-    string public symbol = "MTK"; // symbol of the token
-    uint8 public decimals; // decimals of the token
+    uint256 public totalSupply; // total amount of tokens have been minted.
     address public owner; // the owner of the contract
+    uint8 public decimals; // decimals of the token
+    string public name; // name of the token
+    string public symbol; // symbol of the token
 
     constructor() {
         name = "My Token";
         symbol = "MTK";
         decimals = 18;
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
     }
 
     function transfer(address recipient, uint256 amount)
@@ -70,5 +76,29 @@ contract MyToken is IERC20 {
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
         emit Transfer(msg.sender, address(0x0), amount); // event Transfer is required; the receiver is 0x0
+    }
+
+    // Deposit => Transfer [msg.value] amount of tokens to [msg.sender]
+    function deposit() external payable {
+        balanceOf[msg.sender] += msg.value;
+        totalSupply += msg.value;
+        emit Transfer(address(0x0), msg.sender, msg.value);
+    }
+
+    // Withdraw => Transfer [_amount] of ethers to [msg.sender] and burn [_amount] of tokens
+    function withdraw(uint256 _amount) external {
+        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success);
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+        emit Transfer(msg.sender, address(0x0), _amount);
+    }
+
+    // receive() => Same as deposit()
+    receive() external payable {
+        balanceOf[msg.sender] += msg.value;
+        totalSupply += msg.value;
+        emit Transfer(address(0x0), msg.sender, msg.value);
     }
 }
